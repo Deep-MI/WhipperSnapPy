bd4b679acdeb144ca3dc58e516b7ba1250143965
Martin Reuter <reuter@mit.edu>
2026-03-05 12:57:25 +0100

    fix fmax shadowing and better logging of Nans

--- whippersnappy/geometry/prepare.py
+++ whippersnappy/geometry/prepare.py
@@ -13,6 +13,8 @@ import warnings
 
 import numpy as np
 
+logger = logging.getLogger(__name__)
+
 from ..utils.colormap import binary_color, heat_color, mask_sign, rescale_overlay
 from ..utils.types import ColorSelection
 from .inputs import resolve_annot, resolve_bg_map, resolve_mesh, resolve_overlay, resolve_roi
